632a3776b70db9534cf06ed85303c87c27a2a1ef
Jees Vincent <jeesvincent0@gmail.com>
2026-02-19 10:41:06 +0530

    refactor(auth): verify token return type changed and new error messages add

--- src/modules/auth/application/ports/token/token-service.port.ts
+++ src/modules/auth/application/ports/token/token-service.port.ts
@@ -3,6 +3,6 @@ import { IAccessTokenPayload, IRefreshTokenPayload } from './type';
 export interface TokenServicePort {
   generateAccessToken(payload: IAccessTokenPayload): Promise<string>;
   generateRefreshToken(payload: IRefreshTokenPayload): Promise<string>;
-  verifyRefreshToken(token: string): Promise<any>;
-  verifyAccessToken(token: string): Promise<any>;
+  verifyRefreshToken(token: string): Promise<IRefreshTokenPayload>;
+  verifyAccessToken(token: string): Promise<IAccessTokenPayload>;
 }
--- src/shared/constants/error-messages.ts
+++ src/shared/constants/error-messages.ts
@@ -1,10 +1,17 @@
 export const ERROR_MESSAGES = {
+  // Token error messages
+  INVALID_ACCESS_TOKEN: 'Invalid access token',
+  MISSING_TOKEN: 'Missing Token',
+
+  // OTP verification error messages
   TOO_MANY_ATTEM: 'Too many attempts, try again after sometimes',
   PLEASE_TRY_AGAIN: 'Please try again',
   INVALID_OTP_PURPOSE: 'In valid otp purpose',
+  OTP_EXPIRED: 'OTP has expired',
+
+  // user signup/signin error messeges
   AGE_MUST_BE_12: 'Age must be at least 12 years old',
   USER_ALREADY_EXIST: 'User already exit',
   USER_NOT_FOUND: 'User does not exist',
   INVALID_PASSWORD: 'Invalid password',
-  OTP_EXPIRED: 'OTP has expired',
 } as const;
