2755df32f21904f43dfd1fc7470e66bf7a9cf547
Jees Vincent <jeesvincent0@gmail.com>
2026-02-22 20:07:50 +0530

    feat(auth): google auth user create use case added

--- src/modules/user/application/use-cases/create-users/create-user-with-google-auth.usecase.ts
+++ src/modules/user/application/use-cases/create-users/create-user-with-google-auth.usecase.ts
@@ -1,9 +1,41 @@
+import {
+  Advertiser,
+  BaseUser,
+  Email,
+  User,
+  UserRole,
+} from '@/modules/user/domain';
 import { IGoogleAuthInput } from '../../inputs';
 import { UserRepositoryPort } from '../../ports';
 
 export class CreateUserWithGoogleAuthUseCase {
   constructor(private readonly _userRepo: UserRepositoryPort) {}
-  async execute(input: IGoogleAuthInput) {
-    console.log('This data is from user module: ', input);
+  async execute(input: IGoogleAuthInput): Promise<BaseUser> {
+    const email = Email.create(input.email);
+    let user;
+    if (input.role === UserRole.ADVERTISER) {
+      user = Advertiser.create({
+        firstName: input.firstName,
+        lastName: input.lastName,
+        displayName: input.displayName,
+        email,
+        googleId: input.googleId,
+        isProfileCompleted: false,
+        isVerified: true,
+      });
+    } else if (input.role === UserRole.USER) {
+      user = User.create({
+        firstName: input.firstName,
+        lastName: input.lastName,
+        displayName: input.displayName,
+        googleId: input.googleId,
+        email,
+        isProfileCompleted: false,
+        isVerified: true,
+      });
+    }
+
+    await this._userRepo.save(user);
+    return user as BaseUser;
   }
 }
