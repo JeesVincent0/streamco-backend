19a3a844a3507b33af5d50e48866d0a3d81e5f9b
Jees Vincent <jeesvincent0@gmail.com>
2026-02-26 21:33:07 +0530

    feat(auth): access token guard will check the blacklisted tokens from the cache

--- src/modules/auth/infrastructure/guards/access-token.guard.ts
+++ src/modules/auth/infrastructure/guards/access-token.guard.ts
@@ -5,20 +5,26 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
-import { TOKEN_SERVICE, type TokenServicePort } from '../../application';
+import {
+  TOKEN_BLACK_LIST_CACHE,
+  TOKEN_SERVICE,
+  type TokenServicePort,
+  type BaseCachedUserRepositoryPort,
+} from '../../application';
 import { Request } from 'express';
 import { ERROR_MESSAGES } from '@/shared/constants/error-messages';
+import { UnauthorizedError } from '@/shared/errors';
 
 @Injectable()
 export class AccessTokenGuard implements CanActivate {
   constructor(
+    @Inject(TOKEN_BLACK_LIST_CACHE)
+    private readonly _tokenBlacklistRepo: BaseCachedUserRepositoryPort,
     @Inject(TOKEN_SERVICE) private readonly _tokenService: TokenServicePort,
   ) {}
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest<Request>();
-
     const cookie = request.cookies as Record<string, unknown>;
-    console.log('cookie from accessToken guard: ', cookie);
     const accessToken = cookie?.['accessToken'] as string;
 
     if (!accessToken || typeof accessToken !== 'string') {
@@ -27,6 +33,10 @@ export class AccessTokenGuard implements CanActivate {
 
     try {
       const payload = await this._tokenService.verifyAccessToken(accessToken);
+      const blackList = await this._tokenBlacklistRepo.get(payload.jti);
+      if (blackList) {
+        throw new UnauthorizedError(ERROR_MESSAGES.SESSION_EXPIRED);
+      }
       request['user'] = payload;
       request['accessToken'] = accessToken;
       return true;
